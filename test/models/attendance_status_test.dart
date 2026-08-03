c2188ef0b4f6f6f418a72d53d4670931551bb41e
Willy Tanuwiharja <willy.tanuwiharja@omnisoftsolution.com>
2026-08-03 16:47:53 +0800

    feat: wifi_ssid/wifi_bssid in punch body + network_gate status parsing

--- test/models/attendance_status_test.dart
+++ test/models/attendance_status_test.dart
@@ -24,4 +24,32 @@ void main() {
       expect(s.flexibleLocation, isFalse);
     });
   });
+
+  group('AttendanceStatus network_gate parsing', () {
+    test('parses wifi_required and expected_ssids', () {
+      final s = AttendanceStatus.fromJson({
+        'checked_in': false,
+        'hours_today': 0,
+        'employee_id': 1,
+        'auth_type': 'session',
+        'network_gate': {
+          'wifi_required': true,
+          'expected_ssids': ['Office-WiFi', 'Office-WiFi-5G'],
+        },
+      });
+      expect(s.wifiRequired, isTrue);
+      expect(s.expectedSsids, ['Office-WiFi', 'Office-WiFi-5G']);
+    });
+
+    test('defaults when network_gate absent (old connector)', () {
+      final s = AttendanceStatus.fromJson({
+        'checked_in': false,
+        'hours_today': 0,
+        'employee_id': 1,
+        'auth_type': 'session',
+      });
+      expect(s.wifiRequired, isFalse);
+      expect(s.expectedSsids, isEmpty);
+    });
+  });
 }
